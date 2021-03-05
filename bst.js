436eba7c7b038a5fd228460f6587740a55d65c5e
nemostatus <mattcorralejo1@gmail.com>
2021-03-05 06:48:59 -0800

    adding comments from geeksforgeeks

--- bst.js
+++ bst.js
@@ -42,17 +42,23 @@ class BinarySearchTree{
         }
         // it recur over the tree to find the 
 // data and removes it 
-removeNode(node,key){
-    if(node===null)
+removeNode(node,key){ //so the params are node ===this.root and key===data so removeNode(5,10)
+    if(node===null) 
         return null //if the root is null then return null 
-    else if(key<node.data){
+         // if data to be delete is less than  
+    // roots data then move to left subtree 
+    else if(key<node.data){ 
         node.left =this.removeNode(node.left,key)
         return node
     }
+        // if data to be delete is greater than  
+    // roots data then move to right subtree 
       else if(key>node.data){
         node.right =this.removeNode(node.right,key)
         return node
     }
+     // if data is similar to the root's data  
+    // then delete this node 
     else{
         if(node.left === null && node.right ===null){
             node = null
